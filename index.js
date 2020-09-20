0509ea4d2fdfd57d8b84b05defa45adada77e24b
steven <wuqi0509@gmail.com>
2020-09-20 14:54:40 +0800

    Add static

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 app.use(cors())
+app.use(express.static('build'))
 
 let persons = [
   {
