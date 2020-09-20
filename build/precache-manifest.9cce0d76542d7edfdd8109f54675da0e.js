09e8d31a53da9aeb55f070193a2c952c160aea8f
steven <wuqi0509@gmail.com>
2020-09-20 14:48:21 +0800

    Add front-end build

---
+++ build/precache-manifest.9cce0d76542d7edfdd8109f54675da0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dda9d911b3910b42678ad6c21b93f1c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38270aa1c3249ca056c6",
+    "url": "/static/css/main.14c8b2e5.chunk.css"
+  },
+  {
+    "revision": "6c0f444ebba93378dbff",
+    "url": "/static/js/2.19ed25a5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.19ed25a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38270aa1c3249ca056c6",
+    "url": "/static/js/main.77c03555.chunk.js"
+  },
+  {
+    "revision": "f4d2ac87a76b6331fd91",
+    "url": "/static/js/runtime-main.4234e0d5.js"
+  }
+]);
